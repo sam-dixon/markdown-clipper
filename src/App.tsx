c9d201b4f73ee880957864715fca55066c4be4e6
Sam Dixon <sam.dixon@berkeley.edu>
2022-07-01 21:50:21 -0700

    prototype ux

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear, faX, faStar, faTrash } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
-function App() {
+function DownloadLocation(props: any) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="download-location-container">
+      <FontAwesomeIcon className={props.isDefault ? 'default' : 'option'} icon={faStar} />
+      <div className="path">{props.path}</div>
+      <FontAwesomeIcon icon={faTrash} />
+    </div>
+  );
+}
+
+function Settings(props: any) {
+  const downloadLocations: { path: string; isDefault: boolean }[] = props.downloadLocations;
+  return (
+    <>
+      <div className="setting-header">
+        <div className="subtitle">Download locations</div>
+        <button className="add-download-location">Add</button>
+      </div>
+      <div className="download-locations-list-container">
+        {downloadLocations.map((loc) => {
+          return <DownloadLocation key={loc.path} path={loc.path} isDefault={loc.isDefault} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+function Main(props: any) {
+  const [articleTitle, setArticleTitle] = useState('Placeholder');
+  const [downloadLocation, setDownloadLocation] = useState('default');
+
+  const handleArticleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setArticleTitle(e.target.value);
+  };
+
+  const handleDownloadLocationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setDownloadLocation(e.target.value);
+  };
+
+  return (
+    <>
+      <textarea
+        className="article-title-input"
+        value={articleTitle}
+        onChange={handleArticleTitleChange}
+      />
+      <div className="footer">
+        <select
+          className="download-location-dropdown"
+          value={downloadLocation}
+          onChange={handleDownloadLocationChange}
         >
-          Learn React
-        </a>
-      </header>
+          <option className="download-location-option" value="default">
+            Default
+          </option>
+        </select>
+        <button className="submit-button">Add</button>
+      </div>
+    </>
+  );
+}
+
+function App() {
+  const [isSettingsShown, setIsSettingsShown] = useState(true);
+
+  const downloadLocations = [
+    { path: '~/obsidian/personal/tweets', isDefault: false },
+    { path: '~/obsidian/personal/long_reads', isDefault: true },
+    { path: '~/obsidian/work/docs', isDefault: false },
+  ];
+
+  const toggleSettings = (_e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsSettingsShown(!isSettingsShown);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="title">{isSettingsShown ? 'Settings' : 'Markdown Web Clipper'}</div>
+        <button className="settings-button" onClick={toggleSettings}>
+          {isSettingsShown ? <FontAwesomeIcon icon={faX} /> : <FontAwesomeIcon icon={faGear} />}
+        </button>
+      </div>
+      {isSettingsShown ? <Settings downloadLocations={downloadLocations} /> : <Main />}
     </div>
   );
 }
